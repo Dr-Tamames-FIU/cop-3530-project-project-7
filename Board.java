fd0a88d161c44c7877aa7893f64f04b9d5a391eb
apepitone <apepi004@fiu.edu>
2023-11-28 23:11:36 -0500

    .

--- Board.java
+++ Board.java
@@ -1,6 +1,15 @@
+import java.util.ArrayList;
+
 public class Board {
 
     private char[][] gameBoard;
+
+    private ArrayList<ArrayList<Integer>> wininRows;
+
+    private ArrayList<ArrayList<Integer>> wininColumns;
+
+    private ArrayList<ArrayList<Integer>> wininDiagonals;
+
 
     public Board() {
 
@@ -8,15 +17,19 @@ public class Board {
 
         initializeGameBoard();
 
+        initializeWinnerCheck();
+
+
+
     }
 
     private void initializeGameBoard() {
 
-            for (int i = 0; i < 3; i++) {
+            for (int x = 0; x < 3; x++) {
 
-            for (int j = 0; j < 3; j++) {
+            for (int y = 0; y < 3; y++) {
 
-                gameBoard[i][j] = ' ';
+                gameBoard[x][y] = ' ';
 
             }
 
@@ -24,17 +37,75 @@ public class Board {
 
     }
 
+    private void initializeWinnerCheck() {
+
+        wininRows = new ArrayList<>();
+
+        wininColumns = new ArrayList<>();
+
+        wininDiagonals = new ArrayList<>();
+
+        for (int x = 0; x < 3; x++) {
+
+            ArrayList<Integer> newRow = new ArrayList<>();
+
+                for (int y = 0; y < 3; y++) {
+
+                newRow.add(x * 3 + y);
+
+            }
+
+            wininRows.add(newRow);
+
+        }
+
+
+        for (int x = 0; x < 3; x++) {
+
+            ArrayList<Integer> column = new ArrayList<>();
+
+                for (int y = 0; y < 3; y++) {
+
+                column.add(x * 3 + y);
+
+            }
+
+            wininColumns.add(column);
+
+        }
+
+
+        ArrayList<Integer> primaryDiagonal = new ArrayList<>();
+
+        ArrayList<Integer> secondaryDiagonal = new ArrayList<>();
+
+        for (int x = 0; x < 3; x++) {
+
+            primaryDiagonal.add(x * 3 + x);
+
+            secondaryDiagonal.add(x * 3 + (2 - x));
+
+        }
+
+        wininDiagonals.add(primaryDiagonal);
+
+        wininDiagonals.add(secondaryDiagonal);
+
+    }
+
+
+
     public void displayGameBoard() {
 
         System.out.println("-------------");
 
-            for (int i = 0; i < 3; i++) {
+            for (int x = 0; x < 3; x++) {
 
             System.out.print("| ");
 
-            for (int j = 0; j < 3; j++) {
+            for (int y = 0; y < 3; y++) {
 
-                System.out.print(gameBoard[i][j] + " | ");
+                System.out.print(gameBoard[x][y] + " | ");
 
             }
 
@@ -45,7 +116,9 @@ public class Board {
     }
 
     public boolean isMoveValid(int row, int col) {
+        
         return row >= 0 && row < 3 && col >= 0 && col < 3 && gameBoard[row][col] == ' ';
+
     }
 
     public void makeMove(int row, int col, char playerSymbol) {
@@ -56,21 +129,27 @@ public class Board {
 
     public boolean checkForWin() {
 
-        return checkForWinColumns() || checkForWinDiagonals();
+        return checkForHelp(wininRows) || checkForHelp(wininColumns) || checkForWinDiagonals();
 
     }
 
-    private boolean checkForWinColumns() {
 
-        for (int col = 0; col < 3; col++) {
 
-            if (gameBoard[0][col] != ' ' &&
+    private boolean checkForHelp(ArrayList<ArrayList<Integer>> positions) {
 
-                gameBoard[0][col] == gameBoard[1][col] &&
+        for (ArrayList<Integer> pos : positions) {
 
-                gameBoard[1][col] == gameBoard[2][col]) {
+            char symbol = gameBoard[pos.get(0) / 3][pos.get(0) % 3];
+         
+             if (symbol != ' ') {
 
-                return true;
+                boolean isMatch = pos.stream().allMatch(p -> gameBoard[p / 3][p % 3] == symbol);
+         
+                    if (isMatch) {
+
+                        return true;
+
+                }
 
             }
 
@@ -80,28 +159,14 @@ public class Board {
 
     }
 
+    
+
     private boolean checkForWinDiagonals() {
 
-        if (gameBoard[0][0] != ' ' &&
-        gameBoard[0][0] == gameBoard[1][1] &&
-        gameBoard[1][1] == gameBoard[2][2]) {
-        // If the diagonal from top-left to bottom-right has the same value and is not empty, return true
-        return true;
-    }
- 
-    if (gameBoard[0][2] != ' ' &&
-        gameBoard[0][2] == gameBoard[1][1] &&
-        gameBoard[1][1] == gameBoard[2][0]) {
-        // If the diagonal from top-right to bottom-left has the same value and is not empty, return true
-        return true;
-    }
- 
-    // If no winning diagonal is found, return false
-    return false;
-    
-}
+        return checkForHelp(wininDiagonals);
 
-    
+    }
+
 
     public boolean checkForDraw() {
 
