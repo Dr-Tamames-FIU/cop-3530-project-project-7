e789aaf81b1cf6d118471164be0f28660e5faabe
apepitone <apepi004@fiu.edu>
2023-12-01 02:07:50 -0500

    V3

--- PlayerMap.java
+++ PlayerMap.java
@@ -1,18 +1,20 @@
 import java.util.HashMap;
+
 import java.util.Map;
 
-
+// Shows how player names are mapped to player objects.
 	public class PlayerMap {
  
     private Map<String, Player> gamePlayers;
- 
+
+ // The PlayerMap is initialized with an empty HashMap by the constructor.
     public PlayerMap() {
 
         gamePlayers = new HashMap<>();
 
     }
  
-    
+    // Inserts a new player with the specified name and symbol into the game.
     public void addPlayerToGame(String playerName, char playerSymbol) {
 
         Player gamePlayer = new Player(playerName, playerSymbol);
@@ -21,11 +23,11 @@ import java.util.Map;
 
     }
  
-    
+    // Acquires the Player object linked to the given player identity
     public Player getPlayerFromGame(String playerName) {
 
         return gamePlayers.get(playerName);
 
     }
-    
+
 }
